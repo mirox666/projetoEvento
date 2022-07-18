40fee5fdf546d4b7981a5c99b39a95363c0dd751
Matheus Raposo <123lampiao10@gmail.com>
2022-07-18 20:58:48 -0300

    Iniciando projetoEvento com PHP

---
+++ public/exemplos/sintaxe_basico.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe Básica PHP</title>
+</head>
+<body>
+    <h1>Exemplo de texto</h1>
+
+    <?php 
+        echo "<h1>Olá Mundo</h1>";
+        $variavel = 10;// Criando uma Variável
+        echo "<p>$variavel</p>";
+    ?>
+
+</body>
+</html>
