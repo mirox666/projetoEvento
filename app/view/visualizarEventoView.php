65013692f7dc592ef2a928a510f02660ef7401bb
Matheus Raposo <123lampiao10@gmail.com>
2022-08-23 20:57:14 -0300

    Iniciado a página de visualização

---
+++ app/view/visualizarEventoView.php
@@ -0,0 +1,19 @@
+<?php
+include_once ("../includes/cabecalho.php");
+?>
+
+<main class="container-fluid mt-5">
+    <section class="d-flex justify-content-between">
+        <h3 class="fw-bold">Gerenciamento de Eventos</h3>
+        <a href="CadastroView.php" class="btn btn-primary">Criar Evento</a>
+    </section>
+    <hr>
+
+    <section class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-4">
+
+    </section>
+</main>
+
+<?php
+include_once ("../includes/rodape.php");
+?>
